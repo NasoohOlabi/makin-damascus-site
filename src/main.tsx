47eb73753634f784b47fd16c9fc934c1433beb3e
nasooh <nasooholabi@gmail.com>
2025-03-18 12:13:17 +0300

    try github push

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,36 @@
+import { ThemeProvider } from "@/components/theme-provider.tsx";
+
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
 import "./index.css";
 
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyC4RNZDqX2Wgow-ejC4DqgMJR11VuJQ01s",
+	authDomain: "makeen-damascus.firebaseapp.com",
+	projectId: "makeen-damascus",
+	storageBucket: "makeen-damascus.firebasestorage.app",
+	messagingSenderId: "691045970226",
+	appId: "1:691045970226:web:e5a51f3b6033cf1f89a72b",
+	measurementId: "G-104MQ9P8ES"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log(`analytics = `, analytics);
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,33 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Makin Damascus',
+        short_name: 'Makin',
+        description: 'Makin Damascus - Islamic Center',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
